def63139a8ccab2f42d83a31042032492b2769af
McMax <burrteque@tlen.pl>
2012-08-24 20:34:17 +0200

    Changes in bash scripts

--- extract_mult.bash
+++ extract_mult.bash
@@ -7,7 +7,7 @@ else
 	CHARGE=$1
 	MULT=$2
 
-	./extractor ../Testy_z_danymi/corrs2mult/VMATCHED/mult/$MULT/$CHARGE/ParticleTree_mult$(expr $MULT)_$(expr $CHARGE).root 158 ../Testy_z_danymi/corrs2mult/VMATCHED/pp-158-MC-2009.root NONE && mv Extracted_distributions.root ../Testy_z_danymi/corrs2mult/VMATCHED/mult/$MULT/$CHARGE/pp-158-nonmixed-distr.root
+	./extractor ../Testy_z_danymi/corrs2mult/Dane/mult/$MULT/$CHARGE/ParticleTree_mult$(expr $MULT)_$(expr $CHARGE).root 158 ../Testy_z_danymi/corrs2mult/Dane/pp-158-FULL-2009.root NONE && mv Extracted_distributions.root ../Testy_z_danymi/corrs2mult/Dane/mult/$MULT/$CHARGE/pp-158-nonmixed-distr.root
 
-	./extractor ../Testy_z_danymi/corrs2mult/VMATCHED/mult/$MULT/$CHARGE/MixedParticleTree_mult$(expr $MULT)_$(expr $CHARGE).root 158 ../Testy_z_danymi/corrs2mult/VMATCHED/pp-158-MC-2009.root NONE && mv Extracted_distributions.root ../Testy_z_danymi/corrs2mult/VMATCHED/mult/$MULT/$CHARGE/pp-158-mixed-distr.root
+	./extractor ../Testy_z_danymi/corrs2mult/Dane/mult/$MULT/$CHARGE/MixedParticleTree_mult$(expr $MULT)_$(expr $CHARGE).root 158 ../Testy_z_danymi/corrs2mult/Dane/pp-158-FULL-2009.root NONE && mv Extracted_distributions.root ../Testy_z_danymi/corrs2mult/Dane/mult/$MULT/$CHARGE/pp-158-mixed-distr.root
 fi
