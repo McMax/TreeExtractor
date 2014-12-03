aab73a469bb19a46153b8a7925a4e5497e73bb4f
McMax <wwy_l935@wp.pl>
2014-12-03 20:12:54 +0100

    Changed number of input parameters

--- src/Extractor.cpp
+++ src/Extractor.cpp
@@ -17,25 +17,22 @@ using namespace std;
 
 int main(int argc, char** argv)
 {
-	if(argc == 5)
+	if(argc == 4)
 	{
 		TFile *root_tree_file = new TFile(argv[1]);
 		TTree *particletree = (TTree*)root_tree_file->Get("events");
-		TString root_filename = argv[3];
 		float energy = atof(argv[2]);
-		TString root_output_filename = argv[4];
+		TString root_output_filename = argv[3];
 
 		cout << "Reading file" << endl;
 
-		//Biore rozklad ile eventow mialo dany rozmiar
-		map<int, int> distribution = getDistro(root_filename);
 		mainanalyze(particletree, energy, root_output_filename);
 
 		root_tree_file->Close();
 	}
 	else
 	{
-		cout << "USAGE: extractor <path_to_particle_tree> <energy> <root_file> <outputfile>" << endl;
+		cout << "USAGE: extractor <path_to_particle_tree> <energy> <outputfile>" << endl;
 		return -1;
 	}
 }
