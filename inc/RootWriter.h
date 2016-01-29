a7fad44ef0cf98a63adcec3ea2465989aab3bcf6
McMax <wwy_l935@wp.pl>
2016-01-29 14:35:33 +0100

    New: CALM version of MC

--- inc/RootWriter.h
+++ inc/RootWriter.h
@@ -35,9 +35,6 @@ struct Histos
 	TH1F	*histTheta;
 	TH1F	*histThetaNeg;
 	TH1F	*histThetaPos;
-	TH1F	*histThetacms;
-	TH1F	*histThetacmsNeg;
-	TH1F	*histThetacmsPos;
 
 	TH1F	*histYreal;
 	TH1F	*histYrealNeg;
@@ -45,18 +42,9 @@ struct Histos
 	TH1F	*histYpi;
 	TH1F	*histYpiNeg;
 	TH1F	*histYpiPos;
-	TH1F	*histYcms;
-	TH1F	*histYcmsNeg;
-	TH1F	*histYcmsPos;
-	TH1F	*histYprotcms;
-	TH1F	*histYprotcmsNeg;
-	TH1F	*histYprotcmsPos;
 	TH1F	*histEta;
 	TH1F	*histEtaNeg;
 	TH1F	*histEtaPos;
-	TH1F	*histEtacms;
-	TH1F	*histEtacmsNeg;
-	TH1F	*histEtacmsPos;
 
 	TH1F	*histPtWide;
 	TH1F	*histPtAll;
@@ -65,9 +53,6 @@ struct Histos
 	TH1F	*histPzAll;
 	TH1F	*histPzNeg;
 	TH1F	*histPzPos;
-	TH1F	*histPzcmsAll;
-	TH1F	*histPzcmsNeg;
-	TH1F	*histPzcmsPos;
 	TH1F	*histMeanPt;
 	TH1F	*histMeanPtNeg;
 	TH1F	*histMeanPtPos;
@@ -75,7 +60,6 @@ struct Histos
 
 	TH1F	*histEtot;
 	TH1F	*histEtotCALM;
-	TH1F	*histEtotCALMCMS;
 
 	TH2F	*histPtVsYAll;
 	TH2F	*histPtVsYNeg;
@@ -119,13 +103,12 @@ class Particles
 {
 	Histos *histos;
 	Float_t	angle,
-			theta, theta_cms,
+			theta,
 			mass, y,
-			y_pi, y_pi_cms,
-			y_proton_cms,
-			eta, eta_cms,
-			E_real, E_real_cms, E_pi, E_proton,
-			px, py, pz, p, pt, pz_cms,
+			y_pi,
+			eta,
+			E_real, E_pi, E_proton,
+			px, py, pz, p, pt,
 			mean_pt[3];
 
 	charge		particle_charge;
@@ -133,20 +116,12 @@ class Particles
 	Int_t gpid;
 
 public:
-	static Float_t y_cms;
-	static Float_t beta;
-	static Float_t gamma;
-	static Float_t gamma_beta_e;
 
 	Particles() {}
 
 	void init(Histos *histograms, const float momentum);
 	void newEvent(bool first = false);
 	void analyze(Particle*, const int);
-	
-	static float inline calc_beta(float momentum) { return (momentum/(TMath::Sqrt(momentum*momentum+nucleon_mass*nucleon_mass)+nucleon_mass));}
-	static float inline calc_gamma(float momentum) { return (1/(TMath::Sqrt(1-TMath::Power(calc_beta(momentum),2))));}
-	float inline calc_gbE(float ener) { return (gamma_beta_e = beta*gamma*ener);}
 };
 
 float mk_angle3(float);
